a0e583180185d959310cc57e203c94c3a9e90565
Sonderman <alihaydar338@gmail.com>
2020-02-28 01:46:09 +0300

    Almost Done
    neredeyse bitti

---
+++ Assets/LevelController.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+    private void Update()
+    {
+        foreach(Enemy enemy in _enemies)
+        {
+            if(enemy != null)
+            {
+                return;
+            }
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
